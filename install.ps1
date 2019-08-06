252ff7829a86b34956b19dfd65ab9197508b7830
Sung Rim Huh <sungrimhuh@gmail.com>
2019-08-06 23:19:41 +0900

    install addons automatically

--- install.ps1
+++ install.ps1
@@ -35,5 +35,48 @@ $configPath = "$firefoxRoot\config.js"
 $webClient.DownloadFile($configUrl, $configPath)
 Write-Output 'Downloaded configuration.'
 
+# Download addons.
+
+$addons = @(
+	@{
+		Name = 'ublock-origin';
+		AccountId = '11423598';
+		AddonId = 'uBlock0@raymondhill.net'
+	},
+	@{
+		Name = 'privacy-badger17';
+		AccountId = '5474073';
+		AddonId = 'jid1-MnnxcxisBPnSXQ@jetpack'
+	},
+	@{
+		Name = 'https-everywhere';
+		AccountId = '5474073';
+		AddonId = 'https-everywhere@eff.org'
+	}
+)
+
+$addonUrl = 'https://addons.mozilla.org/en-US/firefox/addon/{0}/'
+
+# The latest addon URL is found on https://stackoverflow.com/a/55593381.
+# {0} is the addon name, the last part of the path of URL to the addon page.
+# {1} is the account ID, the last part of the path of the URL on user link on the addon page.
+$addonDownloadUrl = 'https://addons.mozilla.org/firefox/downloads/latest/{0}/addon-{1}-latest.xpi'
+
+# https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Distribution_options/Sideloading_add-ons
+$addonPath = "$env:APPDATA\Mozilla\Extensions\{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
+
+# Ensure the addon path exists.
+New-Item -Path $addonPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+
+ForEach ($addon in $addons) {
+	Write-Output "Downloading addon `"$($addonUrl -f $addon.Name)`"."
+	$webClient.DownloadFile(
+		($addonDownloadUrl -f $addon.Name, $addon.AccountId),
+		"$addonPath\$($addon.AddonId).xpi"
+	)
+}
+
+Write-Output 'Downloaded addons.'
+
 # Start Firefox.
 Start-Process "$firefoxRoot\firefox.exe"
