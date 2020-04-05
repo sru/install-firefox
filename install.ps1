563aaad092795baefaa05ff8dc0e2ffbcd16f6e4
Sung Rim Huh <sungrimhuh@gmail.com>
2020-04-05 22:47:24 +0900

    fill policies.json

--- install.ps1
+++ install.ps1
@@ -23,18 +23,6 @@ Write-Output 'Installed Firefox.'
 
 $firefoxRoot = 'C:\Program Files\Mozilla Firefox'
 
-# Enable AutoConfig.
-# $autoConfigUrl = 'https://raw.githubusercontent.com/sru/install-firefox/master/autoconfig.js'
-# $autoConfigPath = "${firefoxRoot}\defaults\pref\autoconfig.js"
-# $webClient.DownloadFile($autoConfigUrl, $autoConfigPath)
-# Write-Output 'Enabled AutoConfig.'
-
-# Download configuration.
-# $configUrl = 'https://raw.githubusercontent.com/sru/install-firefox/master/config.js'
-# $configPath = "${firefoxRoot}\config.js"
-# $webClient.DownloadFile($configUrl, $configPath)
-# Write-Output 'Downloaded configuration.'
-
 $distributionPath = "${firefoxRoot}\distribution"
 # Ensure the path exists.
 New-Item -Path $distributionPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
@@ -43,52 +31,38 @@ New-Item -Path $distributionPath -ItemType Directory -ErrorAction SilentlyContin
 $policiesJson = @'
 {
 	"policies": {
-		"DisableMasterPasswordCreation": true
+		"DisableSetDesktopBackground": true,
+		"DisableMasterPasswordCreation": true,
+		"DisableFirefoxAccounts": true,
+		"DisableFormHistory": true,
+		"DisablePocket": true,
+		"DisableProfileImport": true,
+		"DNSOverHTTPS: {
+			"Enabled": true,
+			"Locked": true
+		},
+		"DontCheckDefaultBrowser": true,
+		"EnableTrackingProtection": {
+			"Value": true,
+			"Locked": true,
+			"Cryptomining": true,
+			"Fingerprinting": true
+		},
+		"Extensions": {
+			"Install": [
+				"https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi",
+				"https://addons.mozilla.org/firefox/downloads/latest/https-everywhere/latest.xpi"
+			]
+		},
+		"NoDefaultBookmarks": true,
+		"OfferToSaveLogins": false,
+		"RequestedLocales": ["en-US", "ko-KR"],
+		"SanitizeOnShutdown": true,
 	}
 }
 '@
 Set-Content -Path "${distributionPath}\policies.json" -Encoding ASCII -Value $policiesJson
 Write-Output 'Created policies.json file.'
-
-# Download addons.
-
-$addons = @(
-	@{
-		Name = 'ublock-origin';
-		AccountId = '11423598';
-		AddonId = 'uBlock0@raymondhill.net'
-	},
-	@{
-		Name = 'https-everywhere';
-		AccountId = '5474073';
-		AddonId = 'https-everywhere@eff.org'
-	}
-)
-
-$addonUrl = 'https://addons.mozilla.org/en-US/firefox/addon/{0}/'
-
-# The latest addon URL is found on https://stackoverflow.com/a/55593381.
-# {0} is the addon name, the last part of the path of URL to the addon page.
-# {1} is the account ID, the last part of the path of the URL on user link on the addon page.
-$addonDownloadUrl = 'https://addons.mozilla.org/firefox/downloads/latest/{0}/addon-{1}-latest.xpi'
-
-# https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Distribution_options/Sideloading_add-ons
-# https://support.mozilla.org/en-US/kb/deploying-firefox-with-extensions
-# Sideloading addons is disabled from version 74 and on.
-
-$addonPath = "${distributionPath}\extensions"
-# Ensure the addon path exists.
-New-Item -Path $addonPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
-
-ForEach ($addon in $addons) {
-	Write-Output "Downloading addon `"$($addonUrl -f $addon.Name)`"."
-	$webClient.DownloadFile(
-		($addonDownloadUrl -f $addon.Name, $addon.AccountId),
-		"${addonPath}\$($addon.AddonId).xpi"
-	)
-}
-
-Write-Output 'Downloaded addons.'
 
 # Start Firefox.
 Start-Process "${firefoxRoot}\firefox.exe"
